e7da1c8a0adfcdeb94c39b7c158a33ff959a1010
Lovely <lovelyreal@mail.ru>
2025-10-18 19:22:50 +0300

    Add Encoder class

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.*;
+import java.util.ArrayList;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i ==== " + i);
+public class Main {
+    public static void main(String[] args) throws IOException{
+        StringBuilder pathToDirectory = new StringBuilder();
+        StringBuilder fileName = new StringBuilder();
+        String pathToFile = "C:\\Users\\1\\Documents\\HOMEWORK.txt";
+        boolean flag = false;
+        for (int i = pathToFile.length()-1; i >= 0 ; i--) {
+            if(pathToFile.charAt(i) == '\\') {flag = true;}
+            if(flag){pathToDirectory.append(pathToFile.charAt(i));}
+            else {fileName.append(pathToFile.charAt(i));}
+        }
+        fileName.reverse();
+        pathToDirectory.reverse();
+        System.out.println(fileName);
+        System.out.println(pathToDirectory);
+        InputStreamReader fileReader = new InputStreamReader(new FileInputStream(pathToFile));
+
+        ArrayList<Character> chars = new ArrayList<>(1024);
+        while(fileReader.ready()){
+            chars.add(Character.valueOf((char) fileReader.read()));
+        }
+        for (Character aChar : chars) {
+            System.out.print(aChar);
         }
     }
 }
